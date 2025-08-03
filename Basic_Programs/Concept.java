d568bd8c64f12fd5a05be5c6fb23f2bb01577901
suryansh2216 <suryanshpandeyofc@gmail.com>
2025-08-03 16:39:11 +0530

    Local To Remote

---
+++ Basic_Programs/Concept.java
@@ -0,0 +1,10 @@
+
+public class Concept {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello  in testing phase");
+        System.out.println("Test Test");
+        System.out.println("Println will always start from new line");
+        System.out.print("But print() will not move cursor to next line");
+    }
+}
